e729ea27db11c3cf0ea6145bf5a53d45492fefc9
dualface <dualface@gmail.com>
2014-05-05 11:34:13 +0800

    fix

--- scripts/config.lua
+++ scripts/config.lua
@@ -5,7 +5,14 @@ DEBUG_MEM_USAGE         = false
 DEVICE_ORIENTATION      = "landscape"
 CONFIG_SCREEN_WIDTH     = 960
 CONFIG_SCREEN_HEIGHT    = 640
-CONFIG_SCREEN_AUTOSCALE = "FIXED_HEIGHT_ON_SMALL_SCREEN"
+CONFIG_SCREEN_AUTOSCALE = "FIXED_HEIGHT"
+
+CONFIG_SCREEN_AUTOSCALE_CALLBACK = function(screenWidthInPixels, screenHeightInPixels, deviceModel)
+    CONFIG_SCREEN_WIDTH = screenWidthInPixels
+    CONFIG_SCREEN_HEIGHT = screenHeightInPixels
+    local scaleX, scaleY = 1, 1
+    return scaleX, scaleY
+end
 
 TEXTURE_FORMAT = {}
 TEXTURE_FORMAT["MapA0001Bg.png"]    = kCCTexture2DPixelFormat_RGB565
