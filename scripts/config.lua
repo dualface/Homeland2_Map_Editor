1303ad8d6a9c98025569d3f0fd63584f55da9d33
dualface <dualface@gmail.com>
2014-05-06 17:35:31 +0800

    update proj

--- scripts/config.lua
+++ scripts/config.lua
@@ -1,17 +1,21 @@
 
-DEBUG                   = 2
-DEBUG_FPS               = false
-DEBUG_MEM_USAGE         = false
-DEVICE_ORIENTATION      = "landscape"
-CONFIG_SCREEN_WIDTH     = 960
-CONFIG_SCREEN_HEIGHT    = 640
-CONFIG_SCREEN_AUTOSCALE = "FIXED_HEIGHT"
+DEBUG                     = 2
+DEBUG_FPS                 = false
+DEBUG_MEM                 = true
+
+CONFIG_SCREEN_ORIENTATION = "landscape"
+CONFIG_SCREEN_WIDTH       = 960
+CONFIG_SCREEN_HEIGHT      = 640
+CONFIG_SCREEN_AUTOSCALE   = "FIXED_HEIGHT"
 
 CONFIG_SCREEN_AUTOSCALE_CALLBACK = function(screenWidthInPixels, screenHeightInPixels, deviceModel)
+    if device.platform == "ios" or device.platform == "android" then
+        return nil, nil
+    end
+
     CONFIG_SCREEN_WIDTH = screenWidthInPixels
     CONFIG_SCREEN_HEIGHT = screenHeightInPixels
-    local scaleX, scaleY = 1, 1
-    return scaleX, scaleY
+    return 1, 1
 end
 
 TEXTURE_FORMAT = {}
